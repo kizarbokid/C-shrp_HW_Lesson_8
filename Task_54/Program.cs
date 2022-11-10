a2024204e069d6e51f44cd513587e8d334bb9ece
YakovkinVD <dikobrazzzik709@gmail.com>
2022-11-10 22:02:01 +0900

    task_54 added

---
+++ Task_54/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+System.Console.Clear();
+string ToInputVar(string input_text)
+{
+    Console.Write($"Введите {input_text} и нажмите Enter: ");
+    string result = Console.ReadLine();
+    return result;
+}
+int[,] ToFillTable(int n, int m)
+{
+    int[,] table = new int[n, m];
+    var rand = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        { table[i, j] = rand.Next(0, 10); }
+    }
+    return table;
+}
+void ToPrintTable(int[,] table)
+{
+    int rows = table.GetUpperBound(0) + 1;    // количество строк
+    int columns = table.Length / rows;        // количество столбцов
+    for (int i = 0; i < rows; i++)
+    {
+        Console.Write($"{i + 1}-я:  ");
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{table[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string input_text = "количество строк таблицы";
+int n = int.Parse(ToInputVar(input_text));
+input_text = "количество столбцов таблицы";
+int m = int.Parse(ToInputVar(input_text));
+int[,] table = ToFillTable(n, m);
+ToPrintTable(table);
